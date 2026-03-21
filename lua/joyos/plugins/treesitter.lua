72de2594adc0f83d8fe74a42de9258186aeffff2
Joyos <joyoslive@gmail.com>
2026-03-21 14:02:49 +0100

    add treesitter textobjects

--- lua/joyos/plugins/treesitter.lua
+++ lua/joyos/plugins/treesitter.lua
@@ -1,12 +1,46 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	lazy = false,
-	build = ':TsUpdate',
-	config = function()
-		require 'nvim-treesitter'.setup {
-			-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-			install_dir = vim.fn.stdpath('data') .. '/site'
-		}
-		require('nvim-treesitter').install { 'lua' }
-	end
+	{
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		build = ':TsUpdate',
+		config = function()
+			require 'nvim-treesitter'.setup {
+				-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+				install_dir = vim.fn.stdpath('data') .. '/site'
+			}
+			require('nvim-treesitter').install { 'lua' }
+		end
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			require('nvim-treesitter-textobjects').setup {
+				lookahead = true,
+			}
+
+			local tst_select = require 'nvim-treesitter-textobjects.select'
+
+			vim.keymap.set({ "x", "o" }, "af", function()
+				tst_select.select_textobject("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "if", function()
+				tst_select.select_textobject("@function.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				tst_select.select_textobject("@class.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				tst_select.select_textobject("@class.inner", "textobjects")
+			end)
+			-- You can also use captures from other query groups like `locals.scm`
+			vim.keymap.set({ "x", "o" }, "as", function()
+				tst_select.select_textobject("@local.scope", "locals")
+			end)
+		end,
+	}
 }
