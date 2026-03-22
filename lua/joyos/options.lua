2117fc40f22be748bdfb7918749d2d299f5e4083
Joyos <joyoslive@gmail.com>
2026-03-22 23:17:27 +0100

    add cursorline (highlight current line)

--- lua/joyos/options.lua
+++ lua/joyos/options.lua
@@ -4,10 +4,11 @@ vim.opt.termguicolors = true
 -- Ruler
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.cursorline = true
 
 -- Tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
 -- List chars
